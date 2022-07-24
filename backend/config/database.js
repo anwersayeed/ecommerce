aacd60197d8ccd9de4fb374a2546c251090ae39e
anwersayeed <deeyasrewna@gmail.com>
2022-07-25 00:18:01 +0530

    Wrapped MonogoDB connect code inside a function and imported and called it in the main server file

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,12 @@
 const mongoose  = require("mongoose");
 
-// After Connecting, we call a call back function // if fails, catch error
-mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true}).then((data)=>{
-    console.log(`MongoDB Connected with server: ${data.connection.host}`);
-}).catch((err)=>{
-    console.log(err);
-})
+const connectDatabase = ()=>{
+    // After Connecting, we call a call back function // if fails, catch error
+    mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true}).then((data)=>{
+        console.log(`MongoDB Connected with server: ${data.connection.host}`);
+    }).catch((err)=>{
+        console.log(err);
+    })
+}
+
+module.exports = connectDatabase
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const app = require("./app");
-
 const dotenv = require("dotenv");
+
+const connectDatabase = require("./config/database")
 
 // CONFIG
 dotenv.config({path:"backend/config/config.env"})
+
+// Connecting to databse // this is kept below the config setup above, as it will be used in the below code
+connectDatabase()
+
 
 // CALLBACK FUNCTION
 app.listen(process.env.PORT, ()=>{
