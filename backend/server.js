2f23bfe87d1ada5b7e71524feb9fdbe76ab51c7b
anwersayeed <deeyasrewna@gmail.com>
2022-07-24 23:39:03 +0530

    Added config folder | Added nodemon and attached dev and start scripts

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const app = require("./app");
 
+const dotenv = require("dotenv");
+
+// CONFIG
+dotenv.config({path:"backend/config/config.env"})
+
 // CALLBACK FUNCTION
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
