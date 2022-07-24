f955f590b5f146cc8c026cce6003da6f3395632c
anwersayeed <deeyasrewna@gmail.com>
2022-07-25 00:30:17 +0530

    MongoDB Cloud Database Connected | Remove useCreateIndex when connecting

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose  = require("mongoose");
 
 const connectDatabase = ()=>{
     // After Connecting, we call a call back function // if fails, catch error
-    mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true}).then((data)=>{
+    mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true}).then((data)=>{
         console.log(`MongoDB Connected with server: ${data.connection.host}`);
     }).catch((err)=>{
         console.log(err);
