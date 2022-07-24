1e49f3500383d34ecb642ee9a17ab935e942015c
anwersayeed <deeyasrewna@gmail.com>
2022-07-24 23:51:39 +0530

    Created Basic environment | Created first Api to get all products | With Routes and Controllers

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
-
 const app = express();
+
+app.use(express.json())
+
+// Route Imports
+const product = require("./routes/productRoute");
+
+// Remember, here route is not base_url/api/v1/product, because product is here a variable, go and see, that, you created products, and this work not product
+app.use("/api/v1", product);
 
 module.exports = app
