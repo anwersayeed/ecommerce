041242f57fe4f3b40a5c7b72236af604683cac3f
anwersayeed <deeyasrewna@gmail.com>
2022-07-28 11:17:30 +0530

    Get Single Product Details Route Working

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,6 +23,26 @@ exports.getAllProducts = async (req, res)=>{
     })
 }
 
+// Get Single Products or Get Product Details // why next?
+exports.getProductDetails = async (req, res, next)=>{
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product Not Found"
+        })
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        screen:true,
+        product
+    })
+}
+
 // Update Product -- Admin
 exports.updateProduct = async (req, res, next)=>{
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 const router = express.Router();
 
@@ -8,6 +8,6 @@ router.route("/products").get(getAllProducts);
 router.route("/product/new").post(createProduct);
 
 // as URL of update and delete is same, hence attaching here only // PUT for update, DELETE request to delete
-router.route("/product/:id").put(updateProduct).delete(deleteProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
 
 module.exports = router
